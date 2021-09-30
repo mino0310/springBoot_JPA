0b6802dbe18426f5aaa21192a26808da0b7e8848
mino0310 <bewriter310@gmail.com>
2021-09-30 13:51:49 +0900

    Entity Class Develop

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
@@ -20,5 +22,6 @@ public class Item {
     private int price;
     private int stockQuantity;
 
-
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 }
