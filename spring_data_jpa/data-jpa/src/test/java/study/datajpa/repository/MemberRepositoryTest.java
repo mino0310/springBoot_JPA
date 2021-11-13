17298d20902541d6734140c6b588711a8111aa33
mino0310 <bewriter310@gmail.com>
2021-11-13 10:49:29 +0900

    반환 타입

--- spring_data_jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ spring_data_jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -135,4 +135,14 @@ class MemberRepositoryTest {
         }
 
     }
+
+    @Test
+    public void returnType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> aaa = memberRepository.findListByUsername("AAA");
+    }
 }
