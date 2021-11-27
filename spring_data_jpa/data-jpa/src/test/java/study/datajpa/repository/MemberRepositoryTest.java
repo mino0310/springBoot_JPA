21cf9d9c5c86282fd5c1fe4541075e26c9e3bdeb
mino0310 <bewriter310@gmail.com>
2021-11-27 17:57:06 +0900

    네이티브 쿼리

--- spring_data_jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ spring_data_jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -300,4 +300,25 @@ class MemberRepositoryTest {
 
     }
 
+    @Test
+    public void nativeQuery() {
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        //when
+        Page<MemberProjection> result = memberRepository.findByNativeProjection(PageRequest.of(0, 10));
+        List<MemberProjection> content = result.getContent();
+        for (MemberProjection memberProjection : content) {
+            System.out.println("memberProjection.getUsername() = " + memberProjection.getUsername());
+            System.out.println("memberProjection.getTeamName() = " + memberProjection.getTeamName());
+        }
+    }
 }
